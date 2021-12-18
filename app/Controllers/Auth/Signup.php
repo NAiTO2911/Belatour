f60bad565e9eb843b7a57a524d2c9ff3b5aebd0c
muhfelmi <felmiadi@gmail.com>
2021-12-18 20:56:53 +0700

    x

--- app/Controllers/Auth/Signup.php
+++ app/Controllers/Auth/Signup.php
@@ -1,23 +1,26 @@
-<?php namespace App\Controllers\Auth;
- 
+<?php
+
+namespace App\Controllers\Auth;
+
 use CodeIgniter\RESTful\ResourceController;
 use App\Models\Code_model;
- 
+
 class Signup extends ResourceController
 {
     protected $format       = 'json';
     protected $modelName    = 'App\Models\UserModel';
- 
-    public function index() {
+
+    public function index()
+    {
         $validation =  \Config\Services::validation();
         helper('utils');
-        
+
         $id_user = guidv4();
 
         $full_name = $this->request->getPost('full_name');
         $email = $this->request->getPost('email');
         $activation_code = generateKey($email);
-     
+
         $data_user = [
             'id_user' => $id_user,
             'full_name' => $full_name,
@@ -26,15 +29,15 @@ class Signup extends ResourceController
             'phone_number' => $this->request->getPost('phone_number'),
             'password' => password_hash($this->request->getPost('password'), PASSWORD_DEFAULT)
         ];
-        
+
         $data_code = [
             'id_code' => guidv4(),
             'id_user' => $id_user,
             'key_code' => $activation_code,
             'dt_code_exp' => date('Y-m-d H:i:s', time() + (60 * 5))
         ];
-        
-        if($validation->run($data_user, 'user') == FALSE){
+
+        if ($validation->run($data_user, 'user') == FALSE) {
             $response = [
                 'status' => 500,
                 'error' => true,
@@ -43,10 +46,10 @@ class Signup extends ResourceController
             return $this->respond($response, 500);
         } else {
             $saveUser = $this->model->insertUser($data_user);
-            if($saveUser){
+            if ($saveUser) {
                 $codeModel = new Code_model();
                 $saveCode = $codeModel->insertCode($data_code);
-                if($saveCode){
+                if ($saveCode) {
                     $msg = [
                         'message'       => 'User Account Created Successfully',
                         'confirmation'  => sendMailVerification($email, $full_name, $activation_code)
@@ -61,5 +64,4 @@ class Signup extends ResourceController
             }
         }
     }
-    
 }
--- tests/app/Controllers/TestLogin.php
+++ tests/app/Controllers/TestLogin.php
@@ -18,23 +18,58 @@ class TestLogin extends CIUnitTestCase
         parent::tearDown();
     }
 
-    public function testIndex(){
+    public function testIndex1()
+    {
         $result = $this->post('auth/login', [
-            'id_user' => 'robbyakbar',
-            'password' => '123qwe'
+            'id_user' => 'dev_belatour',
+            'password' => 'beladev'
         ]);
 
-        $result->assertOk();
+        $result->assertOK();
         $result->assertTrue($result->getJSON() !== false);
         $result->assertJSONFragment([
             'status' => 200,
             'error' => false,
             'data' => [
-                'id_user' => "1",
-                'email' => "robbyakbar@upi.edu",
-                'full_name' => "Robby Akbar"
+                'id_user' => "91",
+                'email' => "dev@belatour.com",
+                'full_name' => "Developer Belatour"
             ]
         ]);
     }
 
+    public function testIndex2()
+    {
+        $result = $this->post('auth/login', [
+            'id_user' => 'dev_belatour',
+            'password' => 'beladevs'
+        ]);
+
+        $result->assertOK();
+        $result->assertTrue($result->getJSON() !== false);
+        $result->assertJSONFragment([
+            'status' => 200,
+            'error' => true,
+            'data' => [
+                'message' => "Your Password is Wrong"
+            ]
+        ]);
+    }
+    public function testIndex3()
+    {
+        $result = $this->post('auth/login', [
+            'id_user' => 'dev_belatours',
+            'password' => 'beladevs'
+        ]);
+
+        $result->assertStatus(401);
+        $result->assertTrue($result->getJSON() !== false);
+        $result->assertJSONFragment([
+            'status' => 401,
+            'error' => true,
+            'data' => [
+                'message' => "User Not Found"
+            ]
+        ]);
+    }
 }
